a0439fe672a61efb43061147a6cf02774a9e430d
Omar Lamrabet <omar.lamrabet@vdab.be>
2020-10-05 11:51:32 +0200

    EmailService

---
+++ src/main/java/com/example/jwtimplementation/api/service/EmailService.java
@@ -0,0 +1,45 @@
+package com.example.jwtimplementation.api.service;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+public class EmailService {
+
+
+    private JavaMailSender javaMailSender;
+
+
+
+    public void sendSimpleMessage(String to, String subject, String text){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(text);
+        this.javaMailSender.send(simpleMailMessage);
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+
+        javaMailSender.setUsername("my.gmail@gmail.com");
+        javaMailSender.setPassword("password");
+
+        Properties props = javaMailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return javaMailSender;
+    }
+}
