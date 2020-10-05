f03915726613764869d9f6f6221f613fdb5817cf
Omar Lamrabet <omar.lamrabet@vdab.be>
2020-10-05 12:21:16 +0200

    EmailService

--- src/main/java/com/example/jwtimplementation/JwtimplementationApplication.java
+++ src/main/java/com/example/jwtimplementation/JwtimplementationApplication.java
@@ -1,9 +1,9 @@
 package com.example.jwtimplementation;
 
-import com.example.jwtimplementation.api.config.ApplicationUserRole;
 import com.example.jwtimplementation.api.config.PasswordConfig;
 import com.example.jwtimplementation.api.entities.User;
 import com.example.jwtimplementation.api.repository.UserRepository;
+import com.example.jwtimplementation.api.service.EmailService;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,9 +11,10 @@ import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 import static com.example.jwtimplementation.api.config.ApplicationUserRole.*;
 
@@ -21,10 +22,12 @@ import static com.example.jwtimplementation.api.config.ApplicationUserRole.*;
 public class JwtimplementationApplication {
 	private UserRepository userRepository;
 	private PasswordConfig passwordConfig;
+	private EmailService emailService;
 
-	public JwtimplementationApplication(UserRepository userRepository, PasswordConfig passwordConfig) {
+	public JwtimplementationApplication(UserRepository userRepository, PasswordConfig passwordConfig, EmailService emailService) {
 		this.userRepository = userRepository;
 		this.passwordConfig = passwordConfig;
+		this.emailService = emailService;
 	}
 
 	@PostConstruct
@@ -43,6 +46,11 @@ public class JwtimplementationApplication {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JwtimplementationApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerWhenStarts(){
+		this.emailService.sendEmail("omar_lam@hotmail.com", "Hello Omar, how are you ?", "Test EmailService");
 	}
 
 }
--- src/main/java/com/example/jwtimplementation/api/service/EmailService.java
+++ src/main/java/com/example/jwtimplementation/api/service/EmailService.java
@@ -1,45 +1,26 @@
 package com.example.jwtimplementation.api.service;
 
-import java.util.Properties;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class EmailService {
 
 
     private JavaMailSender javaMailSender;
 
-
-
-    public void sendSimpleMessage(String to, String subject, String text){
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(to);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(text);
-        this.javaMailSender.send(simpleMailMessage);
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
     }
 
-    @Bean
-    public JavaMailSender getJavaMailSender(){
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setPort(587);
-
-        javaMailSender.setUsername("my.gmail@gmail.com");
-        javaMailSender.setPassword("password");
-
-        Properties props = javaMailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-        return javaMailSender;
+    public void sendEmail(String to, String body, String subjet) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject(subjet);
+        simpleMailMessage.setText(body);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setFrom("noreply@vdab.be");
+        this.javaMailSender.send(simpleMailMessage);
+        System.out.println("Email has been send");
     }
 }
